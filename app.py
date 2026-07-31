3a170274b33981a8f42567bc6928367a4e8bf8e9
Isaac Geuel <isaacgeule@gmail.com>
2026-07-31 18:47:35 +0300

    Update favicon path in app.py

--- app.py
+++ app.py
@@ -214,5 +214,5 @@ demo.launch(
     server_name="0.0.0.0",
     server_port=int(os.environ.get("PORT", 7860)),
     pwa=True,
-    favicon_path="static/icon-512.png",  # adjust path to wherever you place the icon
+    favicon_path="icon-512.png",  # adjust path to wherever you place the icon
 )
